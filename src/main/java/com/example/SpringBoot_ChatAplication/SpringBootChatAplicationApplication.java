31a0d181666252932868350bf12e928c9b834f84
aniya <24aniya_io@axiz.co.jp>
2024-06-04 14:21:13 +0900

    HTML・CSS作成

---
+++ src/main/java/com/example/SpringBoot_ChatAplication/SpringBootChatAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_ChatAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_ChatAplication/SpringBootChatAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_ChatAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
